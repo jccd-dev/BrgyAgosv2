079e1f35d87a4beaf06e19afdc84f7728f7eb93a
konnichiwaJCCD <jccd0724@gmail.com>
2023-07-11 21:48:29 +0800

    main layout no changes

--- resources/views/main-layout.blade.php
+++ resources/views/main-layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('agos.ico')}}" type="image/x-icon">
-    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css"> --}}
     <title>@yield('title')</title>
 
     {{-- scripts --}}
