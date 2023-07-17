9a6e05e609e6dd642d447f2f1d2963fe51d8bd2f
konnichiwaJCCD <jccd0724@gmail.com>
2023-07-17 17:52:59 +0800

    added the auto update age functionality

---
+++ app/Helpers/UpdateBirthdays.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Helpers;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use App\Models\Dashboard\ProfilingModel;
+
+class UpdateBirthdays {
+
+    public static function updateAge(){
+
+
+        $update_date = DB::table('is_update')->where('id', 1)->first();
+        if($update_date->update_date){
+            $carbonDate = Carbon::parse($update_date->update_date);
+
+            $formattedMonth = $carbonDate->format('Y-m');
+
+            if($formattedMonth == Carbon::now()->format('Y-m')){
+                return;
+            }
+        }
+
+        $currentMonth = Carbon::now()->format('m');
+        $residents = ProfilingModel::whereRaw("MONTH(dob) = {$currentMonth}")->get();
+
+        foreach($residents as $resident){
+            $resident->age = $resident->age + 1;
+            $resident->save();
+        }
+
+        DB::table('is_update')->where('id', 1)->update([
+            'update_date' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+    }
+}
--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,7 @@ use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
+use App\Helpers\UpdateBirthdays;
 class AdminController extends Controller
 {
     /**
@@ -14,6 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
+        UpdateBirthdays::updateAge();
         return view('adminl-login');
     }
 
